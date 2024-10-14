0f1c43ee969b7cc32616963865a993f7ee47c364
carl0bg <dmitrijkarlysev0493@gmail.com>
2024-10-14 22:12:55 +0300

    add iteration_pk_posts

--- posts/views.py
+++ posts/views.py
@@ -25,10 +25,9 @@ class PostViewSet2(viewsets.ModelViewSet):
     queryset = Posts.objects.all()
     serializer_class = PostSerializer
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.serializer_class(queryset, many= True)
-        for post in serializer.data:
+
+    def iteration_pk_posts(self, serial_data):
+        for post in serial_data:
             posts_photos = PhotoSerializers(
                 Posts.objects.get(id = post['id']).photos.all(),
                 many = True
@@ -44,6 +43,41 @@ class PostViewSet2(viewsets.ModelViewSet):
             post['photos'] = posts_photos.data
             post['documents'] = posts_document.data
             post['videos'] = posts_video.data
+        return serial_data
+
         
-        return Response(serializer.data)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.serializer_class(queryset, many= True)
         
+        return Response(self.iteration_pk_posts(serializer.data))
+
+    def retrieve(self, request, pk=None):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(self.iteration_pk_posts([serializer.data]))
+
+
+    # def retrieve(self, request, pk=None):
+    #     instance = self.get_object()
+    #     serializer = self.get_serializer(instance)
+
+    #     post = serializer.data
+    #     posts_photos = PhotoSerializers(
+    #         Posts.objects.get(id = post['id']).photos.all(),
+    #         many = True
+    #     )
+    #     posts_document = DocumentSerializers(
+    #         Posts.objects.get(id = post['id']).documents.all(),
+    #         many = True
+    #     )
+    #     posts_video = VideoSerializers(
+    #         Posts.objects.get(id = post['id']).videos.all(),
+    #         many = True 
+    #     )
+    #     post['photos'] = posts_photos.data
+    #     post['documents'] = posts_document.data
+    #     post['videos'] = posts_video.data
+        
+    #     return Response(post)
